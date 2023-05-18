43298794b9434c04e63d01c5331ea5983b7d1bc4
nworb9 <emma.nworb@gmail.com>
2023-05-18 11:45:41 -0400

    Init styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./styles.css"
 
 export default function App() {
+    const message = (name) => {
+        return `
+        ${name} is an empress in Yanxi Palace.
+        `
+    }
+
     return (
         <div className="App">
-            <h1>Greetings, Hoifa-Nara</h1>
-            <h2>🎎</h2>
+            <h1>Greetings, Empress 🎎</h1>
+            <h2>{message `Hoifa-Nara`}</h2>
         </div>
     )
 }
